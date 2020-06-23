6c85727a06aefb2ebd42cd19dccda63ae80f2b75
Matteo Carpi <matteocarpi94@gmail.com>
2020-06-23 15:58:51 +0200

    feat: added pubblic

---
+++ public/component---src-pages-404-js-1412366e3c0985a7c5f6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("Bl7J"),u=t("vrFN");n.default=function(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("h1",null,"400 NOT FOUND :("))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-1412366e3c0985a7c5f6.js.map
